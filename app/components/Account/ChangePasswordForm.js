97b0dcdee8e00c8a93a08673d6c3989c3fe422c1
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-03-19 06:30:10 +0000

    only made the component for change password

---
+++ app/components/Account/ChangePasswordForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Button, Input } from 'react-native-elements'
+
+export default function ChangePasswordForm() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
